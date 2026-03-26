00b40d88a14ccd57c19fdc1d490a78779588638b
edgamat <edgamat@outlook.com>
2026-03-26 04:35:57 -0400

    Load app settings when running sample host

--- samples/host/Program.cs
+++ samples/host/Program.cs
@@ -2,8 +2,16 @@ using Microsoft.Extensions.Hosting;
 using Edgamat.Messaging.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Edgamat.Messaging.Samples.Host;
+using Microsoft.Extensions.Configuration;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+// include appsettings.json and environment variables in configuration
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
 builder.ConfigureOpenTelemetry();
 
 builder.Services.AddAzureServiceBus()
